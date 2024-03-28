9b1540db404d6b7491599527135df522c56ec160
Vincent Nys <vincentnys@gmail.com>
2024-03-28 08:39:27 +0100

    Allows toggle between replace and update.

--- index.php
+++ index.php
@@ -236,9 +236,24 @@ switch ($_SERVER['REQUEST_METHOD']) {
         echo html_writer::start_tag('form', array('enctype' => 'multipart/form-data', 'action' => '#', 'method' => 'POST'));
 
         $courses = $DB->get_records('course', []);
+
+        echo html_writer::start_tag('div');
+        echo html_writer::start_tag('input', array('type' => 'radio', 'id' => 'replace', 'name' => 'update_mode', 'value' => 'replace'));
+        echo html_writer::end_tag('input');
+        echo html_writer::start_tag('label', array('for' => 'replace'));
+        echo "Replace (delete existing content)";
+        echo html_writer::end_tag('label');
+
+        echo html_writer::start_tag('input', array('type' => 'radio', 'id' => 'update', 'name' => 'update_mode', 'value' => 'update'));
+        echo html_writer::end_tag('input');
+        echo html_writer::start_tag('label', array('for' => 'update'));
+        echo "Update (retain existing content)";
+        echo html_writer::end_tag('label');
+        echo html_writer::end_tag('div');
+
         echo html_writer::start_tag('div');
         echo html_writer::start_tag('label', array('for' => 'course-select-dropdown'));
-        echo "Import into existing course:";
+        echo "Use existing course:";
         echo html_writer::end_tag('label');
         echo html_writer::start_tag('select', array('id' => 'course-select-dropdown', 'name' => 'course'));
         foreach ($courses as $course) {
@@ -264,137 +279,144 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
 
     case 'POST':
-        $course = $DB->get_record('course', ['id' => intval($_POST['course'])]);
-        $DB->delete_records('course_modules', array('course' => $course->id));
-        $DB->delete_records('course_sections', array('course' => $course->id));
-        if ($_FILES['archive']['type'] === "application/zip" || $_FILES['archive']['type'] === "application/x-zip-compressed") {
-            $zip = new ZipArchive;
-            $open_res = $zip->open($_FILES['archive']['tmp_name']);
-            if ($open_res === TRUE) {
-                $location = '/tmp/' . basename($_FILES['archive']['name']);
-                if (substr($location, -4) === ".zip") {
-                    $location = substr($location, 0, strlen($location) - 4);
-                }
-                $zip->extractTo($location);
-                $zip->close();
-                // create a section for the course "map"
-                $record = new stdClass;
-                $record->course = intval($course->id);
-                $record->section = $next_created_section_number;
-                $next_created_section_number += 1;
-                $record->name = "overview";
-                $record->summary = file_get_contents($location . "/course_structure.svg");
-                $record->summaryformat = 1;
-                $record->sequence = "";
-                $record->visible = 1;
-                $maps_section_id = $DB->insert_record('course_sections', $record);
-                $absolute_svg_path = $location . "/course_structure.svg";
-                $unlocking_contents = file_get_contents($location . "/unlocking_conditions.json");
-                $directory_contents = array_diff(scandir($location), array('..', '.'));
-                $DB->delete_records("clusters", array("courseid" => $record->course));
-                $cluster_ids = array();
-                foreach ($directory_contents as $file) {
-                    if (is_dir($location . "/" . $file)) {
-                        $cluster_record = new StdClass;
-                        $cluster_record->name = $file;
-                        $cluster_record->courseid = $record->course;
-                        $cluster_record->yaml = file_get_contents($location . "/" . $file . "/contents.lc.yaml");
-                        $cluster_ids[$file] = $DB->insert_record("clusters", $cluster_record);
+        $mode = $_POST['update_mode'];
+        if ($mode === 'replace') {
+            $course = $DB->get_record('course', ['id' => intval($_POST['course'])]);
+            $DB->delete_records('course_modules', array('course' => $course->id));
+            $DB->delete_records('course_sections', array('course' => $course->id));
+            if ($_FILES['archive']['type'] === "application/zip" || $_FILES['archive']['type'] === "application/x-zip-compressed") {
+                $zip = new ZipArchive;
+                $open_res = $zip->open($_FILES['archive']['tmp_name']);
+                if ($open_res === TRUE) {
+                    $location = '/tmp/' . basename($_FILES['archive']['name']);
+                    if (substr($location, -4) === ".zip") {
+                        $location = substr($location, 0, strlen($location) - 4);
                     }
-                }
-                $unlocking_conditions = json_decode($unlocking_contents, true);
-                // intended keys: moodle_section_id, manual_completion_assignment_id, assignments
-                $topic_section_produced_metadata = array();
-                foreach ($unlocking_conditions as $key => $value) {
-                    $id_components = explode("__", $key);
-                    $cluster_name = $id_components[0];
-                    $unnamespaced_id = $id_components[1];
-                    $cluster_folder = $location . "/" . $cluster_name;
-                    $raw_cluster_data = file_get_contents($cluster_folder . "/contents.lc.json");
-                    $cluster_metadata = json_decode($raw_cluster_data, true);
-                    $topic_section_consumed_metadata = null;
-                    foreach ($cluster_metadata["nodes"] as $node_metadata) {
-                        if ($node_metadata["id"] === $unnamespaced_id) {
-                            $topic_section_consumed_metadata = $node_metadata;
+                    $zip->extractTo($location);
+                    $zip->close();
+                    // create a section for the course "map"
+                    $record = new stdClass;
+                    $record->course = intval($course->id);
+                    $record->section = $next_created_section_number;
+                    $next_created_section_number += 1;
+                    $record->name = "overview";
+                    $record->summary = file_get_contents($location . "/course_structure.svg");
+                    $record->summaryformat = 1;
+                    $record->sequence = "";
+                    $record->visible = 1;
+                    $maps_section_id = $DB->insert_record('course_sections', $record);
+                    $absolute_svg_path = $location . "/course_structure.svg";
+                    $unlocking_contents = file_get_contents($location . "/unlocking_conditions.json");
+                    $directory_contents = array_diff(scandir($location), array('..', '.'));
+                    $DB->delete_records("clusters", array("courseid" => $record->course));
+                    $cluster_ids = array();
+                    foreach ($directory_contents as $file) {
+                        if (is_dir($location . "/" . $file)) {
+                            $cluster_record = new StdClass;
+                            $cluster_record->name = $file;
+                            $cluster_record->courseid = $record->course;
+                            $cluster_record->yaml = file_get_contents($location . "/" . $file . "/contents.lc.yaml");
+                            $cluster_ids[$file] = $DB->insert_record("clusters", $cluster_record);
                         }
                     }
-                    // TODO: do I really need to mutate *and* return?
-                    $topic_section_produced_metadata = create_course_topic($DB, $course, $key, $topic_section_produced_metadata, $topic_section_consumed_metadata, $cluster_folder . "/" . $unnamespaced_id);
+                    $unlocking_conditions = json_decode($unlocking_contents, true);
+                    // intended keys: moodle_section_id, manual_completion_assignment_id, assignments
+                    $topic_section_produced_metadata = array();
+                    foreach ($unlocking_conditions as $key => $value) {
+                        $id_components = explode("__", $key);
+                        $cluster_name = $id_components[0];
+                        $unnamespaced_id = $id_components[1];
+                        $cluster_folder = $location . "/" . $cluster_name;
+                        $raw_cluster_data = file_get_contents($cluster_folder . "/contents.lc.json");
+                        $cluster_metadata = json_decode($raw_cluster_data, true);
+                        $topic_section_consumed_metadata = null;
+                        foreach ($cluster_metadata["nodes"] as $node_metadata) {
+                            if ($node_metadata["id"] === $unnamespaced_id) {
+                                $topic_section_consumed_metadata = $node_metadata;
+                            }
+                        }
+                        // TODO: do I really need to mutate *and* return?
+                        $topic_section_produced_metadata = create_course_topic($DB, $course, $key, $topic_section_produced_metadata, $topic_section_consumed_metadata, $cluster_folder . "/" . $unnamespaced_id);
 
-                    $node_record = new StdClass;
-                    $node_record->slug = $unnamespaced_id;
-                    $node_record->course_sections_id = $topic_section_produced_metadata[$key]['moodle_section_id'];
-                    $node_record->clusters_id = $cluster_ids[$cluster_name];
-                    $node_record->manual_completion_assignment_id = $topic_section_produced_metadata[$key]['manual_completion_assignment_id'];
-                    var_dump($node_record);
-                    $DB->insert_record("nodes", $node_record);
-                }
-                $namespaced_id_to_completion_id = function ($namespaced_id) use ($topic_section_produced_metadata) {
-                    return $topic_section_produced_metadata[$namespaced_id]['manual_completion_assignment_id'];
-                };
-                $completion_id_to_condition = function ($cm_id) {
-                    return array(
-                        "type" => "completion",
-                        "cm" => $cm_id,
-                        "e" => 1
-                    );
-                };
-                foreach ($unlocking_conditions as $key => $completion_criteria) {
-                    if ($completion_criteria) {
-                        $course_section_id = $topic_section_produced_metadata[$key]['moodle_section_id'];
-                        $course_section_record = $DB->get_record('course_sections', ['id' => $course_section_id]);
-                        $all_type_dependency_completion_ids = array_map($namespaced_id_to_completion_id, $completion_criteria['allOf']);
-                        $one_type_dependency_completion_ids = array_map($namespaced_id_to_completion_id, $completion_criteria['oneOf']);
-                        $all_type_conditions = array_map($completion_id_to_condition, $all_type_dependency_completion_ids);
-                        $one_type_conditions = array_map($completion_id_to_condition, $one_type_dependency_completion_ids);
-                        // Moodle does not apply logical meaning (empty conjunction = true / empty disjunction = false)
-                        // otherwise, conjunction && disjunction would be sufficient
-                        $conjunction = array(
-                            "op" => "&",
-                            "c" => $all_type_conditions,
-                            "show" => true
+                        $node_record = new StdClass;
+                        $node_record->slug = $unnamespaced_id;
+                        $node_record->course_sections_id = $topic_section_produced_metadata[$key]['moodle_section_id'];
+                        $node_record->clusters_id = $cluster_ids[$cluster_name];
+                        $node_record->manual_completion_assignment_id = $topic_section_produced_metadata[$key]['manual_completion_assignment_id'];
+                        var_dump($node_record);
+                        $DB->insert_record("nodes", $node_record);
+                    }
+                    $namespaced_id_to_completion_id = function ($namespaced_id) use ($topic_section_produced_metadata) {
+                        return $topic_section_produced_metadata[$namespaced_id]['manual_completion_assignment_id'];
+                    };
+                    $completion_id_to_condition = function ($cm_id) {
+                        return array(
+                            "type" => "completion",
+                            "cm" => $cm_id,
+                            "e" => 1
                         );
-                        $disjunction = array(
-                            "op" => "|",
-                            "c" => $one_type_conditions,
-                            "show" => false
-                        );
-                        $criteria = [];
-                        if (count($all_type_conditions) > 0 && count($one_type_conditions) > 0) {
-                            $availability = array(
+                    };
+                    foreach ($unlocking_conditions as $key => $completion_criteria) {
+                        if ($completion_criteria) {
+                            $course_section_id = $topic_section_produced_metadata[$key]['moodle_section_id'];
+                            $course_section_record = $DB->get_record('course_sections', ['id' => $course_section_id]);
+                            $all_type_dependency_completion_ids = array_map($namespaced_id_to_completion_id, $completion_criteria['allOf']);
+                            $one_type_dependency_completion_ids = array_map($namespaced_id_to_completion_id, $completion_criteria['oneOf']);
+                            $all_type_conditions = array_map($completion_id_to_condition, $all_type_dependency_completion_ids);
+                            $one_type_conditions = array_map($completion_id_to_condition, $one_type_dependency_completion_ids);
+                            // Moodle does not apply logical meaning (empty conjunction = true / empty disjunction = false)
+                            // otherwise, conjunction && disjunction would be sufficient
+                            $conjunction = array(
                                 "op" => "&",
-                                "c" => [$conjunction, $disjunction],
-                                "showc" => [true, false]
+                                "c" => $all_type_conditions,
+                                "show" => true
                             );
-                        } else if (count($one_type_conditions) > 0) {
-                            $availability = $disjunction;
-                        } else {
-                            // if disjunct is empty (whether there are prerequisites or not)
-                            // the activity cannot be accessed
-                            $availability = array(
+                            $disjunction = array(
                                 "op" => "|",
-                                "c" => [
-                                    array(
-                                        "type" => "profile",
-                                        "sf" => "firstname",
-                                        "op" => "isequalto",
-                                        "v" => "dummy_first_name_to_simulate_false"
-                                    )
-                                ],
+                                "c" => $one_type_conditions,
                                 "show" => false
                             );
+                            $criteria = [];
+                            if (count($all_type_conditions) > 0 && count($one_type_conditions) > 0) {
+                                $availability = array(
+                                    "op" => "&",
+                                    "c" => [$conjunction, $disjunction],
+                                    "showc" => [true, false]
+                                );
+                            } else if (count($one_type_conditions) > 0) {
+                                $availability = $disjunction;
+                            } else {
+                                // if disjunct is empty (whether there are prerequisites or not)
+                                // the activity cannot be accessed
+                                $availability = array(
+                                    "op" => "|",
+                                    "c" => [
+                                        array(
+                                            "type" => "profile",
+                                            "sf" => "firstname",
+                                            "op" => "isequalto",
+                                            "v" => "dummy_first_name_to_simulate_false"
+                                        )
+                                    ],
+                                    "show" => false
+                                );
+                            }
+                            $course_section_record->availability = json_encode($availability);
+                            $DB->update_record('course_sections', $course_section_record);
                         }
-                        $course_section_record->availability = json_encode($availability);
-                        $DB->update_record('course_sections', $course_section_record);
                     }
+                } else {
+                    echo html_writer::start_tag('p') . "Failed to open zip archive." . html_writer::end_tag('p');
                 }
             } else {
-                echo html_writer::start_tag('p') . "Failed to open zip archive." . html_writer::end_tag('p');
+                echo html_writer::start_tag('p') . "File is not recognized as a zip archive." . html_writer::end_tag('p');
             }
+            echo html_writer::start_tag('p') . "Course recreation is complete." . html_writer::end_tag('p');
+            break;
+        } else if ($mode === 'update') {
+            echo html_writer::start_tag('p') . "Update functionality is not available yet." . html_writer::end_tag('p');
         } else {
-            echo html_writer::start_tag('p') . "File is not recognized as a zip archive." . html_writer::end_tag('p');
+            echo html_writer::start_tag('p') . "Unknown usage mode." . html_writer::end_tag('p');
         }
-        echo html_writer::start_tag('p') . "Course recreation is complete." . html_writer::end_tag('p');
-        break;
 }
 echo $OUTPUT->footer();
